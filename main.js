7d423bc9ba6c0058f50a8f74f0766bec633b9f5f
RayzenXF <227984918+RayzenXF@users.noreply.github.com>
2026-02-18 13:40:11 +0100

    Make a paint system but... works only on first square...

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const container = document.querySelector("#container");
 
-for (i = 0; i < 256; i++) {
-    const square = document.createElement("div");
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");   
     square.classList.add("square");
     container.appendChild(square);
 };
+
+const squares = document.querySelector(".square");
+
+squares.addEventListener("mouseover", () => {
+    squares.style.backgroundColor = "black";
+});
