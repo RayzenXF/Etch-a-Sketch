11a45c3ca0909ed381a494eb298328d3ab1318a1
RayzenXF <227984918+RayzenXF@users.noreply.github.com>
2026-02-20 13:03:02 +0100

    New create grid function, more cleaner and comfortable

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 const container = document.querySelector("#container");
 const clearButton = document.querySelector("#clear-btn");
-const gridSize = document.querySelector("#grid-size");
+const gridSizeInput = document.querySelector("#grid-size");
 const gridValueTxt = document.querySelector("#range-value");
+
+function createGrid() {
+    let containerSize = 400;
+    let gridSize = 16;
+    let squareSize = containerSize / gridSize;
+
+    container.innerHTML = "";
+
+    container.style.width = `${containerSize}px`;
+    container.style.height = `${containerSize}px`;
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+        square.style.backgroundColor = "white";
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+    };
+};
+
+createGrid();
